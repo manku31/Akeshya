c281a0b5f5afc432ed19242389ae9c3e1a252b94
Prashant Singh <prashantdineshsingh48@gmail.com>
2024-06-16 19:07:45 +0530

    Add effect in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,25 +2,40 @@ import Image from "next/image";
 
 import officeImg from "../public/images/hero-img.png";
 
+import "./animation.css";
+
 export default function Hero() {
   return (
     <>
-      <div className="flex flex-col-reverse lg:flex-row items-center mx-4 lg:mx-20 mb-10 lg:px-56">
+      <div className="home flex flex-col-reverse lg:flex-row items-center mx-4 lg:mx-20 mb-10 lg:px-56 home">
         <div className="lg:w-1/2 space-y-4">
-          <h1 className="font-semibold text-blue-900 text-2xl lg:text-5xl">
+          <h1
+            className="font-semibold text-blue-900 text-2xl lg:text-5xl"
+            data-aos="fade-up"
+          >
             Grow your business with Akeshya
           </h1>
-          <p className="lg:text-xl">
+
+          <p className="lg:text-xl" data-aos="fade-up" data-aos-delay="400">
             We are team of talented website designers, developers & digital
             marketeers
           </p>
-          <button className="bg-white rounded-full border border-blue-900 hover:bg-blue-900 hover:text-white px-5 py-2 translate-y-full duration-300">
+
+          <button
+            className="bg-white rounded-full border border-blue-900 hover:bg-blue-900 hover:text-white px-5 py-2 translate-y-full duration-300"
+            data-aos="fade-up"
+            data-aos-delay="600"
+          >
             Get Started
           </button>
         </div>
 
-        <div className="lg:w-1/2 m-10">
-          <Image src={officeImg} alt="Office Image" className="mx-auto" />
+        <div className="lg:w-1/2 m-10 image-in-hero" data-aos="fade-left">
+          <Image
+            src={officeImg}
+            alt="Office Image"
+            className="mx-auto animation"
+          />
         </div>
       </div>
     </>

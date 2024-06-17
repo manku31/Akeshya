d717f156208a256f5b70d52d45b50cb64fcb3397
Prashant Singh <prashantdineshsingh48@gmail.com>
2024-06-17 17:52:32 +0530

    Fix the hight and hover effect of cards

--- components/cards/ProcessCard.tsx
+++ components/cards/ProcessCard.tsx
@@ -5,7 +5,7 @@ type ProcessCardProps = {
 
 export default function ProcessCard({ title, description }: ProcessCardProps) {
   return (
-    <div className="group bg-[#f6f6f6] shadow-lg rounded-lg p-5 space-y-5 hover:bg-[#14279b] translate-all duration-300">
+    <div className="group bg-[#f6f6f6] shadow-lg rounded-lg p-5 space-y-5 hover:bg-[#14279b] translate-all duration-300 lg:h-[184px]">
       <h1 className="text-center font-semibold text-lg group-hover:text-white">
         {title}
       </h1>
--- components/cards/ServiceCard.tsx
+++ components/cards/ServiceCard.tsx
@@ -1,3 +1,28 @@
+// import { ElementType } from "react";
+
+// type ServiceCardProps = {
+//   icon: ElementType;
+//   title: string;
+//   description: string;
+// };
+
+// export default function ServiceCard({
+//   icon: Icon,
+//   title,
+//   description,
+// }: ServiceCardProps) {
+//   return (
+//     <div className="group service-card bg-white rounded-xl shadow-lg border border-gray-200 px-5 py-10 lg:max-w-[350px] space-y-5 h-[400px] lg:h-[392px] transition-transform duration-300 transform">
+//       <div className="cross bg-blue-100"></div>
+//       <div className="bg-blue-900 rounded-full p-3 w-14 group-hover:bg-white">
+//         <Icon />
+//       </div>
+//       <h1 className="text-lg font-semibold group-hover:text-white">{title}</h1>
+//       <p className="text-gray-700 group-hover:text-white">{description}</p>
+//     </div>
+//   );
+// }
+
 import { ElementType } from "react";
 
 type ServiceCardProps = {
@@ -12,9 +37,9 @@ export default function ServiceCard({
   description,
 }: ServiceCardProps) {
   return (
-    <div className="group service-card bg-white hover:bg-blue-900 rounded-xl shadow-lg border border-gray-200 px-5 py-10 lg:max-w-[350px] space-y-5 h-[400px] lg:h-auto transition-transform duration-300 transform">
+    <div className="group service-card bg-white rounded-xl shadow-lg border border-gray-200 px-5 py-10 lg:max-w-[350px] space-y-5 h-[400px] lg:h-[392px] transition-transform duration-300 transform">
       <div className="cross bg-blue-100"></div>
-      <div className="bg-blue-900 rounded-full p-3 w-14 group-hover:bg-white">
+      <div className="icon-container bg-blue-900 rounded-full p-3 w-14 group-hover:bg-white">
         <Icon />
       </div>
       <h1 className="text-lg font-semibold group-hover:text-white">{title}</h1>

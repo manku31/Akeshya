e0f02932a493639890a81873ce7fd6896c79e2d2
Prashant Singh <prashantdineshsingh48@gmail.com>
2024-06-17 18:50:00 +0530

    Add configs folder and move aos config

--- configs/AOSInitializer.tsx
+++ configs/AOSInitializer.tsx
@@ -7,8 +7,8 @@ import AOS from "aos";
 export default function AOSInitializer() {
   useEffect(() => {
     AOS.init({
-      duration: 1000, // Customize the duration here
-      once: true, // Whether animation should happen only once - while scrolling down
+      duration: 1000,
+      once: true,
     });
   }, []);
 
